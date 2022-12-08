2778592c50a722a120a200e390545a3b54c80c86
solomonkassa <solomonmulu000@gmail.com>
2022-12-08 08:09:01 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+/*
+ * File: 6-size.c
+ * Auth: Solomon kassa
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the size of various types based on
+ *        the computer it is compiled and run on..
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
+	return (0);
+}
