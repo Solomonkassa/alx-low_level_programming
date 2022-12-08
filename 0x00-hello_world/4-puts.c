f30bb022d6277730942f77d0763ebeb057c4bafe
solomonkassa <solomonmulu000@gmail.com>
2022-12-08 07:58:32 -0800

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+/*
+ * File: 4-puts.c
+ * Auth: Solomon kassa
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
