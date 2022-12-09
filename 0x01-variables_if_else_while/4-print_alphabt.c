da1e1119e7bf1eea61a06f500ddcb9aa03692264
solomonkassa <solomonmulu000@gmail.com>
2022-12-08 23:11:38 -0800

    print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+/*
+ * File: 4-print_alphabt.c
+ * Auth: Solomon kassa
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}i
