ff8726ce0c87f6c21490dc6d3f5857f8a332e98e
solomonkassa <solomonmulu000@gmail.com>
2022-12-08 23:02:02 -0800

    print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+/*
+ * File: 2-print_alphabet.c
+ * Auth: Solomon Kassa
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
