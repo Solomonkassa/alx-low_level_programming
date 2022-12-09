8ba6180235eb3046974133cd000a16ef5695bc17
solomonkassa <solomonmulu000@gmail.com>
2022-12-08 23:37:52 -0800

    print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+/*
+ * File: 9-print_comb.c
+ * Auth: Solomon Kassa
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
