38ba8b67cb11b62f53605991b916d0b29bdeae22
solomonkassa <solomonmulu000@gmail.com>
2022-12-08 23:32:24 -0800

    print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+/*
+ * File: 7-print_tebahpla.c
+ * Auth: Solomon Kassa
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the lowercase alphabet in reverse.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
