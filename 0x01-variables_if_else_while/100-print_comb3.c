b5ae50415e19f7e5288133e323e4b39354e1acee
solomonkassa <solomonmulu000@gmail.com>
2022-12-09 01:19:13 -0800

    betty style

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -21,10 +21,8 @@ int main(void)
 		{
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
-
 			if (digit1 == 8 && digit2 == 9)
-				continue;
-			
+			continue;
 			putchar(',');
 			putchar(' ');
 		}
