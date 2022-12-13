e9fa049f635283e8ebad9d4df2827c7e0959d799
solomonkassa <solomonmulu000@gmail.com>
2022-12-13 03:26:35 -0800

    postive

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,5 @@
-include "main.h"
+#include <stdio.h>
+#include "main.h"
 
 /**
  * main - tests function that prints if integer is positive or negative
@@ -9,7 +10,7 @@ int main(void)
 {
 	int i;
 
-	i = 0;
+	i = 98;
 	positive_or_negative(i);
 
 	return (0);
