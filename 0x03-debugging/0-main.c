e88712ea863a4841aa7e27426abe4b1b4a9b9927
solomonkassa <solomonmulu000@gmail.com>
2022-12-14 22:18:37 -0800

    positive

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,16 @@
 #include "main.h"
+
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
+ * main - Debugging.
+ *
+ * Return: 0
+ */
+
 int main(void)
 {
 	int i;
-	positive_or_negative(98);
 
+	i = 0;
+	positive_or_negative(i);
+	return (0);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,13 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
-int positive_or_negative(int i)
-{
-	if (i < 0)
-		printf("%d is a negative number", i);
-	else if (i > 0)
-		printf("%d is a postive number", i);
-	else
-		printf("%d is 0", i);
-}
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
 #endif /* MAIN_H */
