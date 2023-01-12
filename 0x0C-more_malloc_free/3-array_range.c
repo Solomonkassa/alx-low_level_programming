86b76635314b6740d9059a6c765de8cf1cfe5f4a
solomonkassa <solomonmulu000@gmail.com>
2023-01-12 07:49:26 -0800

    3-arraay

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,11 +14,13 @@ int *array_range(int min, int max)
 	int *ar;
 	int i;
 
-
+	if (min > max)
+		return (NULL);
 
 	ar = malloc(sizeof(*ar) * ((max - min) + 1));
 
-
+	if (ar == NULL)
+		return (NULL);
 
 	for (i = 0; min <= max; i++, min++)
 		ar[i] = min;
