7fc9ee0bc779fa16b19a23968d5ac46a3cda27c1
solomonkassa <solomonmulu000@gmail.com>
2023-01-26 11:37:44 -0800

    yet

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,7 +3,7 @@
 
 /*
  * File: lists.h
- * Auth: Brennan D Baraban
+ * Auth: Solomon Kassa
  * Desc: Header file containing prototypes and definitions for all functions
  *       and types written in the 0x11-singly_linked_lists directory.
  */
