6ef55962cfc5721868daca649d921417a60ff9fe
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-02-01 07:03:06 -0500

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,20 +1,23 @@
-#include "lists.h"
+#include <stdio.h>
+#include <lists.h>
 
 /**
- * get_nodeint_at_index - returns the nth node of a linked list
- * @head: head of a list.
- * @index: index of the node.
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
  *
- * Return: nth node. If node does not exist, returns NULL.
+ * Description: singly linked list node structure
+ * for Alx project
  */
+
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i;
+    unsigned int i;
+    listint_t *node = head;
 
-	for (i = 0; i < index && head != NULL; i++)
-	{
-		head = head->next;
-	}
+    for (i = 0; node && i < index; i++)
+        node = node->next;
 
-	return (head);
+    return (i == index) ? node : NULL;
 }
+
