8f6ae4d225af144f5fe2fd3eb4d04699450a32d7
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-02-01 07:13:54 -0500

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,15 +7,14 @@
  *
  * Return: nth node. If node does not exist, returns NULL.
  */
-
-
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-    unsigned int i;
-   
+	unsigned int i;
 
-    for (i = 0; i < head && index != NULL; i++)
-        node = node->next;
+	for (i = 0; i < index && head != NULL; i++)
+	{
+		head = head->next;
+	}
 
-    return (head) ;
+	return (head);
 }
