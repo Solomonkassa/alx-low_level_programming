8fdb6fa8a981b117a7ca18c8f903047e55ffa7c7
solomonkassa <solomonmulu000@gmail.com>
2023-02-06 11:02:03 -0800

    help

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,11 +1,14 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_
+#define _MAIN_
 
-#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
-#endif /* MAIN_H */
+#endif
